4183ed61844a2839e022a56c012f9bfbd2e6d3e4
Dave Lowensohn <dave@handwovenpdx.com>
2014-05-05 17:18:36 -0700

    Can now accept edits

---
+++ js/models/todo.js
@@ -0,0 +1,22 @@
+Todos.Todo = DS.Model.extend({
+	title: DS.attr('string'),
+	isCompleted: DS.attr('boolean')
+});
+
+Todos.Todo.FIXTURES = [
+{
+	id: 1,
+	title: 'Stay weird',
+	isCompleted: true
+},
+{
+	id: 2,
+	title: 'Avoid confrontation',
+	isCompleted: false
+},
+{
+	id: 3,
+	title: 'Drink too much',
+	isCompleted: false
+}
+];
